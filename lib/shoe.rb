9c0dc93548c792e6b9f30a35b4b6890ba4d7e8fb
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-21 18:33:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand.uniq
+  attr_reader :brand
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS.uniq << brand
   end
 
   def cobble
